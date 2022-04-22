15037ceaf6511c2c19679adcfa328a90ebfdaca7
konstantin-neb <kostyneb36@gmail.com>
2022-04-22 12:49:00 +0300

    home-work-2

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const compareString = (str1, str2) => {
+    return str1.localeCompare(str2, [], { caseFirst: "upper" });
+  };
 
+  return [...arr].sort((str1, str2) =>
+    param === "asc" ? compareString(str1, str2) : compareString(str2, str1)
+  );
 }
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  const newObj = {};
 
+  Object.keys(obj).forEach((field) => {
+    if (!fields.includes(field)) {
+      newObj[field] = obj[field];
+    }
+  });
+
+  return newObj;
 };
