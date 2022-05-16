d893da1f8865e6eadbe9978df379c22eaf440bc6
konstantin-neb <kostyneb36@gmail.com>
2022-05-17 00:17:28 +0300

    (home-work-5)

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,102 @@
-export default class NotificationMessage {
+export const NotificationMessageType = {
+  SUCCESS: 'success',
+  ERROR: 'error'
+};
 
+export default class NotificationMessage {
+  static DEFAULT_TIMEOUT = 3000;
+
+  duration;
+  type;
+  headerText;
+  bodyText;
+  _element;
+
+  constructor(headerText = '', {
+    type = NotificationMessageType.SUCCESS,
+    duration = NotificationMessage.DEFAULT_TIMEOUT,
+    bodyText = '' } = {}
+  ) {
+    this.validateType(type);
+    this.validateDuration(duration);
+
+    this.type = type;
+    this.duration = duration;
+    this.headerText = headerText;
+    this.bodyText = bodyText;
+
+    this._element = this.createElement();
+  }
+
+  static hidePreviousInstance() {
+    const existingNotifications = Array.from(document.getElementsByClassName('notification'));
+    existingNotifications.forEach(notification => notification.remove());
+  }
+
+  get duration() {
+    return this.duration;
+  }
+
+  get element() {
+    return this._element;
+  }
+
+  destroy() {
+    this.remove();
+    this._element = null;
+  }
+
+  remove() {
+    if (!this._element) {
+      return;
+    }
+
+    this._element.remove();
+  }
+
+  show(node = null) {
+    NotificationMessage.hidePreviousInstance();
+    const container = node ?? document.body;
+
+    container.append(this._element);
+
+    this.startDuration();
+  }
+
+  createElement() {
+    const element = document.createElement('div');
+    element.style.setProperty('--value', this.getTimeoutString());
+    element.classList.add('notification');
+    element.classList.add(this.type);
+
+    element.innerHTML = `
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.headerText}</div>
+          <div class="notification-body">${this.bodyText}</div>
+       </div>
+     `;
+
+    return element;
+  }
+
+  getTimeoutString() {
+    return `${this.duration / 1000}s`;
+  }
+
+  startDuration() {
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  validateType(type) {
+    if (!Object.values(NotificationMessageType).includes(type)) {
+      throw new Error('Invalid notification type provided. Only "error" or "success" allowed');
+    }
+  }
+
+  validateDuration(duration) {
+    if (typeof duration !== 'number' || isNaN(duration) || !isFinite(duration)) {
+      throw new Error('Invalid duration provided');
+    }
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,169 @@
-export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+import EventObserver from "../../lib/event-observer.js";
+import Header from "./components/header/header.js";
+import {stringComparator} from "./utils/string.comparator.js";
+import {numberComparator} from "./utils/number.comparator.js";
+import Row from "./components/row.js";
 
+export const SortDirection = {
+  ASC: 'asc',
+  DESC: 'desc'
+};
+
+export const SortType = {
+  NUMBER: 'number',
+  STRING: 'string',
+};
+
+export default class SortableTable {
+  static comparators = {
+    [SortType.STRING]: stringComparator,
+    [SortType.NUMBER]: numberComparator
+  };
+
+  _sort;
+  header;
+  body;
+  data;
+  config;
+  sortObserver = new EventObserver();
+  _element;
+
+  constructor(headerConfig = [], data = []) {
+    this.data = data;
+    this.config = this.getConfig(headerConfig);
+
+    this.header = new Header(headerConfig);
+
+    this.setSortObservers();
+
+    this._element = this.createElement();
+  }
+
+  get element() {
+    return this._element;
+  }
+
+  get subElements() {
+    return {
+      body: this.body
+    };
+  }
+
+  createElement() {
+    const element = document.createElement('div');
+    element.classList.add('sortable-table');
+
+    element.append(this.header.element);
+
+    this.body = document.createElement('div');
+    this.body.classList.add('sortable-table__body');
+    this.body.setAttribute('data-element', 'body');
+
+    this.body.append(...this.createRows().map(row => row.element));
+
+    element.append(this.body);
+
+    return element;
+  }
+
+  sort(id, direction) {
+    this._sort = {
+      type: this.config[id]?.sortType ?? SortType.STRING,
+      id,
+      direction
+    };
+
+    this.sortObserver.notify(this._sort);
+  }
+
+  destroy() {
+    this.remove();
+    this._element = null;
+    this.sortObserver = null;
+  }
+
+  remove() {
+    if (!this._element) {
+      return;
+    }
+    this._element.remove();
+  }
+
+  setSortObservers() {
+    this.sortObserver.subscribe((sort = null) => {
+      this.header.updateSort(sort);
+      this.body.innerHTML = '';
+      if (!sort) {
+        this.body.append(...this.createRows().map(row => row.element));
+        return;
+      }
+
+      const comparator = this.getComparator();
+      if (!comparator) {
+        throw new Error('sort was not provided');
+      }
+
+      const data = [...this.data];
+      data.sort((rowA, rowB) => comparator(rowA[sort.id], rowB[sort.id]));
+
+      this.body.append(...this.createRows(data).map(row => row.element));
+    });
+  }
+
+  getConfig(headerConfig) {
+    this.validateConfig(headerConfig);
+    return headerConfig.reduce((config, item) => {
+      const { id, ...rest } = item;
+      config[id] = rest;
+      return config;
+    }, {});
+  }
+
+  getComparator() {
+    if (!this._sort) {
+      return;
+    }
+    return SortableTable.comparators[this._sort.type](this._sort.direction);
+  }
+
+  validateConfig(headerConfig) {
+    if (!headerConfig.every((item) => !!item.id)) {
+      throw new Error('Each column must have an id field');
+    }
+
+    if (!headerConfig.reduce((isValid, item) => {
+      if (!('sortType' in item)) {
+        return isValid;
+      }
+
+      if (!Object.values(SortType).includes(item.sortType)) {
+        isValid = false;
+      }
+
+      return isValid;
+    }, true)) {
+      throw new Error('Invalid sortType provided');
+    }
+  }
+
+  createRows(data = null) {
+    return (data ?? this.data).map((row) => {
+      const cells = this.getRowData(row);
+      return new Row(cells);
+    });
+  }
+
+  getRowData(row) {
+    return Object.keys(this.config)
+      .filter(id => !!this.config[id])
+      .reduce((resultRow, id) => {
+        resultRow[id] = {
+          id,
+          value: row[id],
+          template: this.config[id].template
+        };
+
+        return resultRow;
+      }, {});
   }
 }
-
